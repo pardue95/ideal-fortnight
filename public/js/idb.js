d4a368eb9672b13f984baaf3bb8a81bf00c6787c
pardue95 <85760640+pardue95@users.noreply.github.com>
2022-02-06 18:07:48 -0600

    indexDB saving

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,40 +1,34 @@
 // create variable to hold db connection
 let db;
-// establish a connection to IndexedDB database called 'transaction' and set it to version 1
-const request = indexedDB.open("transaction", 1);
+// establish a connection to IndexedDB database
+const request = indexedDB.open("budget_tracker", 1);
 
-// this event will emit if the database version changes (nonexistant to version 1, v1 to v2, etc.)
+// this event will emit if the database version changes
 request.onupgradeneeded = function (event) {
-  // save a reference to the database
   const db = event.target.result;
-
-  // create an object store (table) called `new_transaction`, set it to have an auto incrementing primary key of sorts
   db.createObjectStore("new_transaction", { autoIncrement: true });
-
-  // upon a successful
-  request.onsuccess = function (event) {
-    // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
-    db = event.target.result;
-
-    // check if app is online, if yes run uploadTransaction() function to send all local db data to api
-    if (navigator.onLine) {
-      // we haven't created this yet, but we will soon, so let's comment it out for now
-      // uploadTransaction();
-    }
-  };
-
-  request.onerror = function (event) {
-    // log error here
-    console.log(event.target.errorCode);
-  };
 };
 
-// This function will be executed if we attempt to submit a new transaction and there's no internet connection
+// upon a successful
+request.onsuccess = function (event) {
+  // when db is successfully created with its object store
+  db = event.target.result;
+  // check if app is online
+  if (navigator.onLine) {
+    uploadTransaction();
+  }
+};
+
+request.onerror = function (event) {
+  console.log(event.target.errorCode);
+};
+
+// Will be executed if we attempt to submit a new transaction and there's no internet connection
 function saveRecord(record) {
   // open a new transaction with the database with read and write permissions
   const transaction = db.transaction(["new_transaction"], "readwrite");
 
-  // access the object store for `new_transaction`
+  // access the object store
   const budgetObjectStore = transaction.objectStore("new_transaction");
 
   // add record to your store with add method
